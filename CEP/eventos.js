a8ef1489724cc598d8bf43d204aab40d7261e0ef
LucasDCoelho <jessicamaiaaraujo@gmail.com>
2023-03-01 09:13:45 -0300

    aula 22

---
+++ CEP/eventos.js
@@ -0,0 +1,8 @@
+function buscarCEP() {
+    //document.getElementById('logradouro')
+    logradouro.value = "Av. Santos Dummont";
+    bairro.value = "Good Garden";
+    cidade.value = "Caucaia";
+    estado.value = "CE";
+}
+
