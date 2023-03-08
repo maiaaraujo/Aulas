7b3fe0a7c7b7087399ab5f9d6b68f8faa934f2da
edmilsonmiranda <jessicamaiaaraujo@gmail.com>
2023-03-08 09:10:20 -0300

    exercicio

---
+++ Aula 27/Exercício/js/listar.js
@@ -0,0 +1,16 @@
+function item(texto) {
+    return `<li>${texto}</li>`
+}
+
+function listar() {
+    return `
+    <hr>
+    <ul>
+        <li>Giselle</li>
+        <li>Sara</li>
+        <li>Vanessa</li>
+        <li>Sam</li>
+    </ul>
+    `;
+    
+}
---
+++ Aula 27/Exercício/js/main.js
@@ -0,0 +1,8 @@
+import listar from "./listar.js";
+import cadastro from "./cadastro.js";
+import listar from "./listar.js";
+
+function alterarConteudo(x) {
+
+     conteudo.innerHTML = window[x]();
+}
