0248b23e4059242b8dfa54b5ed1b07878d4dbdb7
edmilsonmiranda <jessicamaiaaraujo@gmail.com>
2023-03-10 09:55:14 -0300

    navbar

--- Aula 29/rotas/src/components/Header.jsx
+++ Aula 29/rotas/src/components/Header.jsx
@@ -13,16 +13,15 @@ export default function Header()
 
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                
+                <Link className="navbar-brand" to="/">Home</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
                  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-                    <li className="nav-item">
-                    <Link className="nav-link" to="/">Home</Link>
-                    </li>
+                
                     <li className="nav-item">
                     <Link className="nav-link" to="/alunos">Alunos</Link>
                     </li>
